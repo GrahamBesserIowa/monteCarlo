c5097698f73c40770a76ab2a6d25a2455999836a
GrahamBesserIowa <123344179+GrahamBesserIowa@users.noreply.github.com>
2023-01-23 14:48:07 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,44 +1,69 @@
-import numpy as np
-import matplotlib.pyplot as plt1
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+from matplotlib.patches import Rectangle
+import random
+
+def piSim():
+    #n is number of simulations
+    #the imaginary area range is 20 units wide and 10 units tall
+    #the square is 2x2 units and its center is at (5, 5)
+    #the circle has a radius of 2 unit and its center is at (15,5)
+    cPlot = []
+    n = 1000
+    squareCount = 0
+    circleCount = 0
+    history = []
+    #current ratio is y and x in number of sims
+    t = 0
+    ratio = []
+
+    #these are the radnomly generated plots
+    while n != t:
+        cPlot = [random.random() * 20, random.random() * 10]
+        history.append(cPlot)
+
+        #check if random plot is in sqaure
+        if (7 > cPlot[0] > 3) and (7 > cPlot[1] > 3):
+            squareCount += 1
+
+        #check if random plot is in circle
+        elif ( pow(cPlot[0] - 15,2) + pow(cPlot[1] - 5,2) < 16):
+            circleCount += 1
+
+        t += 1
+
+        if squareCount != 0:
+            ratio.append(circleCount/squareCount)
 
 
-class Pricing:
-    import numpy as np
-    import matplotlib.pyplot as plt
+    return [history,ratio, circleCount, squareCount]
 
-    def priceSim(S, T, r, q, sigma, steps, N):
-        """
-        Inputs
-        #S = Current stock Price
-        #K = Strike Price
-        #T = Time to maturity 1 year = 1, 1 months = 1/12
-        #r = risk free interest rate
-        #q = dividend yield
-        # sigma = volatility
+run = piSim()
 
-        Output
-        # [steps,N] Matrix of asset paths
-        """
-        dt = T / steps
-        # S_{T} = ln(S_{0})+\int_{0}^T(\mu-\frac{\sigma^2}{2})dt+\int_{0}^T \sigma dW(t)
-        ST = np.log(S) + np.cumsum(((r - q - sigma ** 2 / 2) * dt + sigma * np.sqrt(dt) * np.random.normal(size=(steps, N))), axis=0)
+plots1 = run[0]
+plots2 = run[1]
+circleCount = run[2]
+squareCount = run[3]
 
-        return np.exp(ST)
+plt.subplot(2, 1, 1)
+plt.scatter(*zip(*plots1), .25, 'black')
+plt.xlabel("X Units")
+plt.ylabel("Y Units")
+plt.title("Monte Carlo Pi Plots")
+circle = Circle((15,5),4, fill=False)
+plt.gca().add_patch(circle)
+rect = Rectangle((3,3), 4, 4, fill=False)
+plt.gca().add_patch(rect)
+width = 700
+height = 700
+plt.axis('equal')
+plt.axis([0, 20, 0, 10])
 
-    S = 100  # stock price S_{0}
-    K = 110  # strike
-    T = 1 / 2  # time to maturity
-    r = 0.05  # risk free risk in annual %
-    q = 0.02  # annual dividend rate
-    sigma = 0.25  # annual volatility in %
-    steps = 100  # time steps
-    N = 1000  # number of trials
 
-    paths = priceSim(S, T, r, q, sigma, steps, N)
+plt.subplot(2, 1, 2)
+plt.plot(plots2, 'black')
+plt.xlabel("Simulations")
+plt.ylabel("Circle to Square Ratio")
+plt.title("Monte Carlo Pi Ratio")
 
-    plt1.plot(paths)
-    plt1.xlabel("Time Increments")
-    plt1.ylabel("Stock Price")
-    plt1.title("Pricing Sim")
-
-    plt1.show()
+plt.show()
